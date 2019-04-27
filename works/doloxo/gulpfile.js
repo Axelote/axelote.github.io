7ec1ade1583097a2b78dafed57b35ad2baf28c74
Ivan Chepiha <chepiha.ivan@gmail.com>
2019-04-27 18:30:35 +0300

    html

--- works/doloxo/gulpfile.js
+++ works/doloxo/gulpfile.js
@@ -3,14 +3,27 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
  
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+gulp.task('sass',  done => {
+  return gulp.src('./sass/core.scss') 
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 4 versions'],
+      cascade: true
+    }))
     .pipe(rename('styles.css'))
     .pipe(gulp.dest('./css'));
+    done();
 });
  
-gulp.task('sass:watch', function () {
+gulp.task('sass:watch', done => {
   gulp.watch('./sass/**/*.scss', gulp.series('sass'));
+  done();
 });
+
+gulp.task('default', gulp.parallel(
+  'sass',
+  'sass:watch'
+)
+);
